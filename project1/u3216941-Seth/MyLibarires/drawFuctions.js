1cf9ca55be62955e1754725daf11feee55ef93c6
Sethie2001 <seth.arnould@gmail.com>
2020-09-15 09:45:45 +1000

    Start of Project 1

---
+++ project1/u3216941-Seth/MyLibarires/drawFuctions.js
@@ -0,0 +1,35 @@
+function drawHead()
+{
+    // fuction drawHead()
+    ellipse(500, 500,500, 644);
+    fill (black);
+    // ellipse(posleft, postop, width, height)
+    // fill(colour)
+    // Call drawEars
+
+}
+function drawEyes()
+{
+
+
+}
+function drawNose()
+{
+
+
+}
+function drawMouth()
+{
+
+
+}
+function drawhair()
+{
+
+
+}
+function drawBody()
+{
+
+
+}
---
+++ project1/u3216941-Seth/Portraitproject.js
@@ -0,0 +1,19 @@
+function setup() {
+    createCanvas(1000, 1000);
+    background(50);
+  }
+  
+  function draw() {
+drawHead();
+drawEyes();
+drawNose();
+drawMouth();
+drawhair();
+drawBody();
+// Call drawHead()
+// Call drawEyes()
+// Call drawNose()
+// Call drawMouth()
+// Call drawhair()
+// Call drawBody()
+  }
