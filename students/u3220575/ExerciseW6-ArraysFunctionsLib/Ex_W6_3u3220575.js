0e05de51cd847edc344231edcc01aae18fa5d689
LeaRozenn <lea.jehanno@icloud.com>
2020-09-14 15:32:19 +1000

    commitweek7

---
+++ students/u3220575/ExerciseW6-ArraysFunctionsLib/Ex_W6_3u3220575.js
@@ -0,0 +1,13 @@
+// printing function
+function printme(printsomething) {
+    document.write(whatever you want);
+
+}
+
+// product function
+function product(string1, string2){
+    return string1 * string 2;
+}
+
+// functions in functions
+printme(product(3,4));
