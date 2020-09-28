a082c816f646c13e719241fd5134f6ff8e4a9eda
NextQueenLiz <ebazila@gmail.com>
2020-09-28 12:29:24 +1000

    project commit

--- students/u3203648/Project 1/PortraitJS.js
+++ students/u3203648/Project 1/PortraitJS.js
@@ -4,7 +4,10 @@
 var Haircolour;
 var Skincolour;
 var Facecolour;
-
+var Shirtcolour;
+var Collarcolour;
+var Eyecolour;
+var Lipcolour;
 
 function setup() {
   createCanvas(1000, 1000);
@@ -12,13 +15,18 @@ function setup() {
   Haircolour = '#3D270B'
   Skincolour = '#E8D6BF'
   Facecolour = '#F6E4CC'
+  Shirtcolour = 192
+  Collarcolour = 200
+  Eyecolour = '#FFFDF7'
+  Eyecolour2 = '#197D91'
+  Lipcolour = '#FFB7B7'
 }
 
 function draw() {
   background('#0DB689');
-  
-  fill (Haircolour)
+
   noStroke()
+  fill (Haircolour)
   //hair
   beginShape();
   //top vertex
@@ -39,24 +47,95 @@ function draw() {
   vertex(600,320);
   endShape(CLOSE);
     
+  //shirt collar behind left
+  fill(Collarcolour)
+  beginShape()
+  curveVertex(435,760);
+  curveVertex(430,780);
+  curveVertex(380,740);
+  curveVertex(450,710);
+  endShape(CLOSE);
+  
+  //shirt collar behind right
+  beginShape()
+  curveVertex(565,760);
+  curveVertex(570,780);
+  curveVertex(620,740);
+  curveVertex(550,710);
+  endShape(CLOSE);
+  
   //neck
   fill (Skincolour)
   quad(420,640,580,640,560,800,440,800);
-  quad (460,700,540,700,150,900,850,900);
-  
-  //ears
-  
-  //earrings
+  quad(460,700,540,700,190,880,810,880);
   
   //body
   fill(Skincolour)
-  quad(150,900,850,900,760,780,240,780);
+  quad(150,950,850,950,760,780,240,780);
   
   //clothing
+  fill(Shirtcolour)
+  beginShape()
+  //left side
+  vertex(490,950);
+  vertex(400,770);
+  vertex(240,780);
+  vertex(163,870);
+  vertex(158,870);
+  vertex(145,900);
+  vertex(240,950);
+  endShape(CLOSE);
+  
+  beginShape()
+   //right side
+  vertex(510,950);
+  vertex(600,770);
+  vertex(760,780);
+  vertex(837,870);
+  vertex(842,870);
+  vertex(855,900);
+  vertex(760,950);
+  endShape(CLOSE);
+  
+  //shirt collar base left
+  fill(Collarcolour)
+  beginShape()
+  vertex(380,740);
+  vertex(420,760);
+  vertex(490,950);
+  endShape(CLOSE);
+  
+  //shirt collar base right
+  beginShape()
+  vertex(620,740);
+  vertex(580,760);
+  vertex(510,950);
+  endShape(CLOSE);
+  
+  //shirt collar point left
+  fill(Collarcolour);
+  beginShape()
+  vertex(435,820);
+  vertex(380,740);
+  vertex(350,790);
+  vertex(390,780);
+  endShape(CLOSE);
+  
+  //shirt collar point right
+  beginShape()
+  vertex(565,820);
+  vertex(620,740);
+  vertex(650,790);
+  vertex(610,780);
+  endShape(CLOSE);
+  
+   //ears
+  
+  //earrings
   
   //face
   fill (Facecolour);
-  ellipse(500,530,250,370);
+  ellipse(500,530, 250,370);
   
   //hair over face
   fill (Haircolour);
@@ -94,15 +173,132 @@ function draw() {
   endShape(CLOSE);
   
   //eye base
+  fill(Eyecolour);
+  beginShape();
+  vertex(535,525);
+  vertex(540,510);
+  vertex(580,505);
+  vertex(600,520);
+  vertex(570,529);
+  endShape(CLOSE);
   
-  //eyebrows
+  beginShape();
+  vertex(465,525);
+  vertex(460,510);
+  vertex(420,505);
+  vertex(400,520);
+  vertex(430,529);
+  endShape(CLOSE);
+  
+  stroke(0)
+  strokeWeight(2)
+  line(540,510,580,505);
+  line(580,505,595,515);
+  line(420,505,405,515);
+  line(460,510,420,505);
+  
+  //left eyebrow
+  fill(Haircolour);
+  beginShape();
+  vertex(480,492);
+  vertex(475,480);
+  vertex(420,470);
+  vertex(395,490);
+  vertex(420,480);
+  endShape(CLOSE);
+  
+  //right eyebrow
+  beginShape();
+  vertex(520,492);
+  vertex(525,480);
+  vertex(580,470);
+  vertex(605,490);
+  vertex(580,480);
+  endShape(CLOSE);
   
   //eye detail
+  strokeWeight(1)
+  fill(Eyecolour2);
+  beginShape();
+  curveVertex(440,509);
+  curveVertex(455,510);
+  curveVertex(453,520);
+  curveVertex(440,525);
+  curveVertex(428,520);
+  curveVertex(428,508);
+  endShape(CLOSE);
+  
+  beginShape();
+  curveVertex(560,509);
+  curveVertex(545,510);
+  curveVertex(547,520);
+  curveVertex(560,525);
+  curveVertex(572,520);
+  curveVertex(572,508);
+  endShape(CLOSE);
+  
+  fill(0)
+  ellipse(559,513,12);
+  ellipse(441,513,12);
+  
+  //nose
+  stroke(140)
+  strokeWeight(1)
+  line(515,510,520,560);
+  line(523,580,530,590);
+  line(530,590,523,595);
+  line(470,590,477,595);
+  line(477,580,470,590);
   
   //glasses
   
-  //nose
+ let RGlasses = map(mouseX, 0, width, 0, 255)
+ let GGlasses = map(mouseY, 0, height, 0, 255);
+
+  noFill();
+  stroke(RGlasses, GGlasses, 100);
+  strokeWeight(4);
+  beginShape();
+  curveVertex(520,520);
+  curveVertex(521,521);
+  curveVertex(532,548);
+  vertex(570,558);
+  vertex(605,535);
+  vertex(600,493);
+  vertex(540,495);
+  endShape(CLOSE);
+  
+  line(393,505,380,500);
+  curve(530,580,520,520,480,520,470,580)
+  line(607,505,620,500);
+  
+  beginShape();
+  curveVertex(480,520);
+  curveVertex(479,521);
+  curveVertex(468,548);
+  vertex(430,558);
+  vertex(395,535);
+  vertex(400,493);
+  vertex(460,495);
+  endShape(CLOSE);
   
   //mouth
+  noStroke();
+  fill(Lipcolour);
+  beginShape();
+  vertex(550,650);
+  vertex(542,648);
+  vertex(520,635);
+  vertex(500,638);
+  vertex(480,635);
+  vertex(458,648);
+  vertex(450,650);
+  curveVertex(500,665);
+  endShape(CLOSE);
+  
+  noFill();
+  stroke('#FF9898')
+  curve(450,640,490,649,510,649,550,640);
+
   
 }
