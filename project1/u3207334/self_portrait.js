d37ecb1288d809af3368307f4da3ae7a86df7635
DeCingGu <u3207334@uni.canberra.edu.au>
2020-09-30 02:16:55 +1000

    week9_2

---
+++ project1/u3207334/self_portrait.js
@@ -0,0 +1,40 @@
+function setup() {
+    createCanvas(400, 600);
+  }
+  
+  function draw() {
+    
+    
+  }
+
+
+//   Function drawASelf_portrait
+function drawASelf_portrait() {
+
+    // CALL drawFace
+    drawFace();
+    // CALL drawHair
+    drawHair();
+    // CALL drawFeatures
+    drawFeatures();
+    // CALL drawGlasses
+    drawGlasses();    
+    }
+    
+    //fuction drawface
+    function drawFace() {
+        fill('red');
+        ellipse(100,100);
+    
+    }
+    
+    // Function drawFeatures
+    // CALL drawEyes
+    // CALL drawEyebrow
+    // CALL drawNose
+    // CALL drawMouth
+    // CALL drawBlush
+    function drawFeatures() {
+    
+    }
+
