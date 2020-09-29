2b3490acd79428f8fe548d8dfc30ace9de1feb46
SushipingMcMeow <sushiping42@gmail.com>
2020-09-29 15:32:28 +1000

    1st

--- students/u3189665/Project 1/progect1js.js
+++ students/u3189665/Project 1/progect1js.js
@@ -1,24 +1,24 @@
-let y = 100;
-let x = 100;
 
-// The statements in the setup() function
-// execute once when the program begins
+
 function setup() {
-  // createCanvas must be the first statement
-  createCanvas(400, 400);
-  stroke(255); // Set line drawing color to white
-  frameRate(30);
+    createCanvas(400, 400);
+    stroke(255);
+    frameRate(30);
+    let y = 100;
+    let x = 100;
 }
-// The statements in draw() are executed until the
-// program is stopped. Each statement is executed in
-// sequence and after the last line is read, the first
-// line is executed again.
+
 function draw() {
-  let x = width / 2;
-  background(0); // Set the background to black
-  y = y - 2;
-  if (y < 0) {
-    y = height;
-  }
-  circle(x, y, width / 10);
+    background(0);
+    if (y < 0) {
+        y = height;
+    }
+    circle(x, y, width / 10);
+    //drawEye(x, y);
 }
+
+// function drawEye(x, y, size) {
+//     fill(240, 240, 240);
+//     circlex(x, y + 50, width / 10)
+
+// }
