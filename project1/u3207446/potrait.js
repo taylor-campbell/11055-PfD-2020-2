9901109c382e4fa18a95c50af88a5fc32fabf49c
Manish <manishghimire@hotmail.com>
2020-09-30 12:39:44 +1000

    body and face layout completed

--- project1/u3207446/potrait.js
+++ project1/u3207446/potrait.js
@@ -1,5 +1,5 @@
-var x = 600; // Width of Canvas
-var y = 1000; // Height of Canvas
+var x = 800; // Width of Canvas
+var y = 800; // Height of Canvas
 
 
 // A0 - Creating a Canvas
@@ -13,18 +13,81 @@ function draw() {
     // Draw background
     drawBackground();
 
-    // Draw self portrait
-    drawSelfPortrait();
+    //Draw portrait
+    drawPortrait();
 }
 
+
+
+
+
 // A1- Function to draw a background
-function drawBackground(){
+function drawBackground() {
+
+    background(25, 255, 250);
 
 };
 
-// A2 - Function to draw a self portrait
-function drawSelfPortrait(){
+// A2 - Function to draw a portrait
+function drawPortrait() {
+
+    // A3 - Draw body
+    drawBody();
+    // A4 - Draw face
+    drawFace();
+    // A5 - Draw tshirt
+    drawTshirt();
+    // A6 - Draw jacket
+    drawJacket();
+    // A7 - Draw hair
+    drawHair();
+    // A8 - Draw eyes
+    drawEyes();
+    // A9 - Draw nose
+    drawNose();
+    // A10 - Draw mouth
+    drawMouth();
 
 };
+
+// A3 - Function to draw body
+function drawBody() {
+
+    beginShape();
+    vertex(0, 770);
+    vertex(260, 690);
+    vertex(300, 660);
+    vertex(294, 458);
+    vertex(500, 460);
+    vertex(500, 660);
+    vertex(540, 690);
+    vertex(798, 770);
+    endShape(CLOSE);
+
+}
+
+// A4 - Function to draw face
+function drawFace() {
+
+    beginShape();
+    vertex(400, 0);
+    vertex(220, 70);
+    vertex(160, 200);
+    vertex(230, 520);
+    vertex(300, 600);
+    vertex(370, 640);
+    vertex(420, 640);
+    vertex(500, 600);
+    vertex(570, 520);
+    vertex(640, 200);
+    vertex(556, 70);
+    endShape(CLOSE);
+
+}
+
+
+
+
+
 
 
