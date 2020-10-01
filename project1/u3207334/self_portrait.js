94b41db76420cd499a74cf7e75ec14c06ae72945
DeCingGu <u3207334@uni.canberra.edu.au>
2020-10-02 02:20:30 +1000

    project1_js_update

--- project1/u3207334/self_portrait.js
+++ project1/u3207334/self_portrait.js
@@ -1,23 +1,35 @@
+var x=250
+var y=200
 function setup() {
-  createCanvas(800, 800);
-  background(255, 178, 40);
-  strokeWeight(5);
-}
-
-function draw() { 
-
-
- fill(225);
-  ellipse(100, 100, 100, 100);
-
-}
-function setup() {
+  //create canvas
   createCanvas(800,700);
 }
 
 function draw() {
+  
+  //create background
   background(255,178, 40);
+  //set stroke weight
   strokeWeight(3);
+  //create text 'CLICK' for interaction hint
+  fill('black')
+  textFont('Helvetica');
+  textSize(50);
+  text('CLICK', 10,50);
+  
+  //hair1
+  fill('black');
+  rectMode(CENTER);
+  rect(400,350 ,450,550, 300, 300);
+  
+  //clothes
+  fill(97, 142, 193)
+  rect(400,700,300,300,100);
+  
+  //neck
+  fill(197, 140, 133)
+  rect(400,500,100,200,50);
+  
   //face
   fill(197, 140, 133);
   ellipseMode(CENTER);
@@ -25,27 +37,60 @@ function draw() {
   
   //eyes
   ellipseMode(CENTER);
-  fill('black');
-  ellipse(320, 300, 80, 60);
-  ellipse(480, 300, 80, 60);
+  fill('0');
+  ellipse(320, 300, 80, 60);//lift eye
+  ellipse(480, 300, 80, 60);//right eye
+  fill('black')
+  ellipse(320, 300, 50);//lift eye
+  ellipse(480, 300, 50);//right eye
   
-  //
+  
+  //glasses  
+  noFill();
+  rect(320, 300,100,70,20);
+  line(370,300,430,300);
+  rect(480, 300,100,70,20);
   
   //eyebrows
- ellipseMode(CENTER);
   fill('black');
-  ellipse(320, 250, 80, 10);
-  ellipse(480, 250, 80, 10);
+  ellipseMode(CENTER);
+  ellipse(480, 200, 80, 10);
+  if (mouseIsPressed) {
+    ellipse(320, x, 80, 10);
+  } else {
+    ellipse(320, y, 80, 10);
+  }
+
+  //nose
+  fill(197, 140, 133)
+  triangle(380, 350, 420, 350, 400, 320);
+
+  //hair2
+    fill('black');
+  rectMode(CENTER);
+  rect(400,130 ,200, 90, 300, 300);
   
-  
-  
-  	
-  
-  //month
+  //mouth
   fill(245, 149, 185)
-   arc(400, 380, 100, 200, 0, PI, CHORD);
+   
+  if (mouseIsPressed) {
+    ellipse(400,400,50);
+  } else {
+    arc(400, 380, 100, 200, 0, PI, CHORD);
+    
+  }
   
- 
+  //text;?
+  if (mouseIsPressed) {
+  fill(128, 14, 3);
+  translate(500,100)
+  rotate(PI / 4.0);
+  textSize(200);
+  text('?', 0, 0);
+  } else {
+    
+    
+  }
 
  
 }
