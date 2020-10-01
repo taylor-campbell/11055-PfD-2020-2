d390177d6b5e066c419cbdd843a4d47c37ef7bb3
SushipingMcMeow <sushiping42@gmail.com>
2020-10-01 16:40:37 +1000

    IDK some commit

--- students/u3189665/Project 1/progect1js.js
+++ students/u3189665/Project 1/progect1js.js
@@ -14,8 +14,8 @@ function draw() {
         y = height;
     }
     fill('rgb(30, 30, 30)');
-    drawEye(x + 50, y + 50, 50);
-    drawEye(x + 200, y + 50, 50);
+    drawEye(417, 443, 70);
+    drawEye(553, 440, 70);
     fill('rgb(240, 222, 204)');
     beginShape();
     vertex(373, 606);
@@ -27,15 +27,30 @@ function draw() {
     vertex(410, 247);
     vertex(547, 248);
     vertex(626, 340);
-    vertex(624, 460);
+    vertex(629, 455);
     vertex(634, 449);
     vertex(635, 488);
     vertex(617, 544);
     vertex(613, 595);
     vertex(522, 696);
     vertex(461, 699);
+    beginContour();
+    vertex(405,454);
+    vertex(432,451);
+    vertex(451,448);
+    vertex(428,432);
+    vertex(398,433);
+    vertex(385,449);
+    endContour();
+    beginContour();
+    vertex(521,445);
+    vertex(545,450);
+    vertex(568,450);
+    vertex(581,441);
+    vertex(565,430);
+    vertex(544,429);
+    endContour();
     endShape(CLOSE);
-    
 }
 
 function drawEye(x, y, size) {
@@ -53,26 +68,26 @@ function drawEye(x, y, size) {
     if (mouseX < x) {
         if (mouseY > y) {
             xPos = x + size / 3 * (Math.sin(angle) * -xOff);
-            yPos = y + size / 3 * (Math.cos(angle) * yOff);
+            yPos = y + size / 3 * (Math.cos(angle) * yOff/2);
         }
         else {
             xPos = x - size / 3 * (Math.sin(angle) * -xOff);
-            yPos = y - size / 3 * (Math.cos(angle) * -yOff);
+            yPos = y - size / 3 * (Math.cos(angle) * -yOff/2);
         }
     }
     else {
         if (mouseY >= y) {
             xPos = x + size / 3 * (Math.sin(angle) * xOff);
-            yPos = y + size / 3 * (Math.cos(angle) * yOff);
+            yPos = y + size / 3 * (Math.cos(angle) * yOff/2);
         }
         else {
             xPos = x - size / 3 * (Math.sin(angle) * xOff);
-            yPos = y - size / 3 * (Math.cos(angle) * -yOff);
+            yPos = y - size / 3 * (Math.cos(angle) * -yOff/2);
         }
     }
-    circle(xPos, yPos, size / 3 * 2);
+    circle(xPos, yPos, size / 5 * 2);
     fill(30);
-    circle(xPos, yPos, size / 3);
+    circle(xPos, yPos, size / 5);
 
     return;
 }
