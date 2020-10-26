9dc025ed2def3b3c226e7185659f6a27b6146231
Sethie2001 <seth.arnould@gmail.com>
2020-10-26 13:42:25 +1100

    commit

---
+++ project2/Seth/Mainscript.js
@@ -0,0 +1,57 @@
+// /* 
+// START
+
+// SET ibsnList as Default
+
+// CALL isbnList
+
+// CALL isbnSearch
+
+// CREATE selectButton variabe
+
+// CREATE searchInput variable
+
+// CREATE isbnSearch function
+
+// CREATE noMovieAvaliable function
+
+// IF bookInfo in isbnLIST
+
+//           CALL selectButton
+
+//           CALL bookInfo
+
+//           CALL moreInfo
+
+//                  IF moreInfo selected
+
+//                         CALL isbnMovie
+
+// ELSE
+
+//         CALL isbnSearch
+
+//         RECIEVE searchInput
+
+//         CHECK searchInput with ISBN database
+        
+//                IF inputed search not in isbn (incorrect search)
+
+//                          CALL searchFunction
+
+//                 ELSE
+
+//                          CALL bookInfo
+
+//                           CALL moreInfo
+
+//                                      IF moreInfo selected && movie avaliable
+
+//                                             CALL isbnMovie 
+
+//                                      ELSE
+
+//                                              CALL noMovieAvaliable
+
+// END
+ */
